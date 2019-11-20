fa26398ada1e83704181687f39d2923cb7ced0c0
Guillaume Scheibel <gscheibel@expedia.com>
2019-11-20 16:24:16 +0000

    Introduce 2 Thread blocking functions

--- src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
+++ src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
@@ -7,8 +7,25 @@ class ScheduleDetails(val greetings: String) {
     private val logger = LoggerFactory.getLogger(ScheduleDetails::class.java)
 
     fun talks(): List<String> {
-        logger.info("Returning list of talks")
+        logger.info("Getting list of talks")
 
-        return listOf("GraphQL is awesome", "GraphQL-Kotlin is even better")
+        val cached = cachedTalks()
+        val databased = databaseTalks()
+
+        logger.info("Returning list of talks")
+        return listOf(cached, databased).flatten()
+    }
+
+    private fun cachedTalks(): List<String> {
+        Thread.sleep(100)
+        logger.info("Got cached talks")
+        return listOf("GraphQL is awesome")
+    }
+
+
+    private fun databaseTalks(): List<String> {
+        Thread.sleep(3000)
+        logger.info("Got DB talks")
+        return listOf("GraphQL-Kotlin is even better")
     }
 }
