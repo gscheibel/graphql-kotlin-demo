dbc6a553cbcdf8087ad1feceeeaacb0c815895dd
Guillaume Scheibel <gscheibel@expedia.com>
2019-11-20 16:03:21 +0000

    Move the Schedule talks as seperate function to demo fun execution

--- src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
+++ src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
@@ -1,3 +1,14 @@
 package com.expediagroup.graphql.demo.query
 
-class ScheduleDetails(val greetings: String, val talks: List<String>)
+import org.slf4j.LoggerFactory
+
+class ScheduleDetails(val greetings: String) {
+
+    private val logger = LoggerFactory.getLogger(ScheduleDetails::class.java)
+
+    fun talks(): List<String> {
+        logger.info("Returning list of talks")
+
+        return listOf("GraphQL is awesome", "GraphQL-Kotlin is even better")
+    }
+}
