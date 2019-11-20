a63e70ab0ce04648505540b9659832d823fd8b22
Guillaume Scheibel <gscheibel@expedia.com>
2019-11-20 16:31:10 +0000

    Refactor to use coroutines and async functions

--- src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
+++ src/main/kotlin/com/expediagroup/graphql/demo/query/ScheduleDetails.kt
@@ -1,30 +1,35 @@
 package com.expediagroup.graphql.demo.query
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 
 class ScheduleDetails(val greetings: String) {
 
     private val logger = LoggerFactory.getLogger(ScheduleDetails::class.java)
 
-    fun talks(): List<String> {
-        logger.info("Getting list of talks")
+    suspend fun talks(): List<String> = coroutineScope {
+        logger.info("Starting to return the list of talks")
 
-        val cached = cachedTalks()
-        val databased = databaseTalks()
 
+        val cached = async { cachedTalks() }
+        val databased = async { databaseTalks() }
+
+        val results = listOf(cached.await(), databased.await()).flatten()
         logger.info("Returning list of talks")
-        return listOf(cached, databased).flatten()
+        results
     }
 
-    private fun cachedTalks(): List<String> {
-        Thread.sleep(100)
+    private suspend fun cachedTalks(): List<String> {
+        delay(100)
         logger.info("Got cached talks")
         return listOf("GraphQL is awesome")
     }
 
 
-    private fun databaseTalks(): List<String> {
-        Thread.sleep(3000)
+    private suspend fun databaseTalks(): List<String> {
+        delay(3000)
         logger.info("Got DB talks")
         return listOf("GraphQL-Kotlin is even better")
     }
